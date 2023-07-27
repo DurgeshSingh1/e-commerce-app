2852ededcc0db556a73ca143990a8dc88ddac32d
Durgesh <durgeshsingh18101999@gmail.com>
2023-07-27 15:22:24 +0530

    Frontend
    Adding frontend

---
+++ client/src/pages/Policy.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const Policy = () => {
+  return (
+    <Layout title={"Privacy Policy"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactus.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>We keep your information safe</p>
+          <p>We do not give your information to third parties</p>
+          <p>We keep your information for better user experience</p>
+
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Policy;
